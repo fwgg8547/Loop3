4c6321aee4d922a10b25c09c7bf762beaea998f1
fwgg8547 <fwgg8547@gmial.com>
2017-02-12 18:16:51 +0900

    map3 mod

--- main/java/com/fwgg8547/loop2/model/BlockMap3.java
+++ main/java/com/fwgg8547/loop2/model/BlockMap3.java
@@ -4,40 +4,26 @@ import com.fwgg8547.loop2.GameConfig;
 
 public class BlockMap3
 {
-	private static final String TAG = BlockMap2.class.getSimpleName();
+  private static final String TAG = BlockMap2.class.getSimpleName();
 
-	private BlockItem[][] mBoardArray;
+  private BlockItem[][] mBoardArray;
 	
-	public static final BlockMap3 INSTANCE = new BlockMap3();
+  public static final BlockMap3 INSTANCE = new BlockMap3();
 
-	private BlockMap3(){}
+  private BlockMap3(){}
 
-	public void initialize(){
-		mBoardArray = new BlockItem[GameConfig.MAPHEIGHT][GameConfig.MAPWIDTH];
-	}
+  public void initialize(){
+    mBoardArray = new BlockItem[GameConfig.MAPHEIGHT][GameConfig.MAPWIDTH];
+  }
 
-	public void setTop(BlockItem item){
-		System.arraycopy(top, 0, mTopItems, 0, mTopItems.length);
+  public int setItem2EmptySpace(BlockItem item){
+  }
 
-		//
-		for(int i=1; i < GameConfig.MAPHEIGHT; i++){
-			mRightItems[i-1] = mRightItems[i];
-		}
-		mRightItems[GameConfig.MAPHEIGHT-1] = mTopItems[GameConfig.MAPINITIALW-1];
-	}
+  public BlockItem getItem(int x, int y){
+    return null;
+  }
 
-	public BlockItem[] getTop(){
-		return mTopItems;
-	}
-
-	public void setRight(BlockItem[] right){
-		System.arraycopy(right, 0, mRightItems, 0, mRightItems.length);
-
-	}
-
-	public BlockItem[] getRight(){
-		return mRightItems;
-	}
-
-	
+  private Point getEmptyPosition() {
+    return null;
+  }
 }
